e2bdf618bd0fb5413f659e6383dec6dd0cccf814
esbazhin <esbazhin@edu.hse.ru>
2021-03-14 10:51:01 +0300

    controller done

--- PITask2/Controllers/NotesController.cs
+++ PITask2/Controllers/NotesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using PITask2;
 
 namespace PITask2.Controllers
@@ -14,17 +15,29 @@ namespace PITask2.Controllers
     public class NotesController : ControllerBase
     {
         private readonly NotesDbContext _context;
+        private readonly int N;
 
-        public NotesController(NotesDbContext context)
+        public NotesController(NotesDbContext context, IConfiguration config)
         {
             _context = context;
+            N = config.GetValue<int>("N");
         }
 
         // GET: api/Notes
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Note>>> GetNotes()
+        public async Task<ActionResult<IEnumerable<Note>>> GetNotes([FromQuery] string query)
         {
-            return await _context.Notes.ToListAsync();
+            var res = query is null ? await _context.Notes.ToListAsync() : await _context.Notes.Where(n => n.Title.Contains(query) || n.Content.Contains(query)).ToListAsync();
+
+            foreach (var r in res)
+            {
+                if(string.IsNullOrEmpty(r.Title))
+                {
+                    r.Title = new string(r.Content.Take(N).ToArray());
+                }
+            }
+
+            return res;
         }
 
         // GET: api/Notes/5
@@ -38,18 +51,20 @@ namespace PITask2.Controllers
                 return NotFound();
             }
 
+            if (string.IsNullOrEmpty(note.Title))
+            {
+                note.Title = new string(note.Content.Take(N).ToArray());
+            }
+
             return note;
-        }
+        }      
 
         // PUT: api/Notes/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutNote(int id, Note note)
+        public async Task<IActionResult> PutNote(int id, [FromBody]Note note)
         {
-            if (id != note.Id)
-            {
-                return BadRequest();
-            }
+            note.Id = id;
 
             _context.Entry(note).State = EntityState.Modified;
 
